558d2eff203fae3eee6808289dbe6ff8c50c54ad
polomanky <simon.masson@live.ca>
2023-11-07 14:03:11 -0500

    Ajout des définitions dans m_joueur_nim.c

---
+++ m_joueur_nim.c
@@ -0,0 +1,16 @@
+#include "m_joueur_nim.h"
+int get_partie_joueur_en_cours(void) {
+
+}
+
+int init_partie_joueur(const t_partie_infos* partie) {
+
+}
+
+int declencher_coup_joueur(const t_partieID) {
+
+}
+
+void terminer_partie_joueur(const t_partieID) {
+
+}
